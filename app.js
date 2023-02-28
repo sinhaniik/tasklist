8c04e3f25ec0e2f4f7efe241bc1ed060d0acd99a
Nikhil Sinha <nikhilsinha198@gmail.com>
2023-02-28 18:14:13 +0530

    add layouts in ejs and also include work page

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ app.use(express.static('public'));
 // THE ITEM ARRAY THIS IS FOR WHEN THE USER ADDED NEW ITEM TO THE LIST
 let items = [];
 
+// FOR WORK REALTED ITEMS TO BE ADDED
+let workList = [];
+
 // WHEN PAGE GETS LOADED THIS BLOCL OF CODE EXECUTES
 app.get('/', (req, res) => {
 	let value = {
@@ -26,14 +29,36 @@ app.get('/', (req, res) => {
 
 	const todayDate = d.toLocaleDateString('en-US', value);
 
-	res.render('list', { todayDay: todayDate, newItem: items });
+	res.render('list', { mainHeading: todayDate, newItem: items });
 });
 
 // TO POST IT TO THE SERVER
 app.post('/', (req, res) => {
+	const list = req.body.list;
+
+	// TO CHECK THE TYPE OF LIST SO THAT I CAN DECIDE IT BELONGS TO THE HOME ROUTE OR /work ROUTE
+	if (list === 'Work List') {
+		const name = req.body.name;
+		workList.push(name);
+		res.redirect('/work');
+	} else {
+		const name = req.body.name;
+		items.push(name);
+		res.redirect('/');
+	}
+});
+
+// MAKE WORK ROUTE SO THAT THE USER CAN LIST THEIR WORK ACCORDINGLY
+
+app.get('/work', (req, res) => {
+	// HERE LIST IS THE PAGE NAME IN THIS CONTEXT list.ejs
+	res.render('list', { mainHeading: 'Work List', newItem: workList });
+});
+
+app.post('/work', (req, res) => {
 	const name = req.body.name;
-	items.push(name);
-	res.redirect('/');
+	workList.push(name);
+	res.redirect('/work');
 });
 
 app.listen(3000, () => {
